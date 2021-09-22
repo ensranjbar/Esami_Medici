7c8997e105c35fb1af380af0db045fe972e70834
ensranjbar <55926968+ensranjbar@users.noreply.github.com>
2021-09-22 12:33:32 +0200

    .logout

--- siw-spring-security/src/main/java/it/uniroma3/siw/authentication/AuthConfiguration.java
+++ siw-spring-security/src/main/java/it/uniroma3/siw/authentication/AuthConfiguration.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import javax.sql.DataSource;
 
@@ -63,6 +64,7 @@ public class AuthConfiguration extends WebSecurityConfigurerAdapter {
                 .and().logout()
                 // il logout è attivato con una richiesta GET a "/logout"
                 .logoutUrl("/logout")
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
                 // in caso di successo, si viene reindirizzati alla /index page
                 .logoutSuccessUrl("/index")        
                 .invalidateHttpSession(true)
