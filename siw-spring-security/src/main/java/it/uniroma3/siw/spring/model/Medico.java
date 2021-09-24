e168d155555338722fce533934cdc3666ba61ed8
ensranjbar <55926968+ensranjbar@users.noreply.github.com>
2021-09-24 12:45:10 +0200

    medico + tipologia  photos

--- siw-spring-security/src/main/java/it/uniroma3/siw/spring/model/Medico.java
+++ siw-spring-security/src/main/java/it/uniroma3/siw/spring/model/Medico.java
@@ -1,5 +1,6 @@
 package it.uniroma3.siw.spring.model;
 
+import java.beans.Transient;
 import java.util.List;
 
 import javax.persistence.CascadeType;
@@ -10,7 +11,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
-
 import lombok.Data;
 
 @Data
@@ -19,17 +19,27 @@ public class Medico {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
-	
+
 	@Column(nullable = false)
 	private String nome;
-	
+
 	@Column(nullable = false)
 	private String cognome;
-	
+
 	@Column(nullable = false)
 	private String Specializzazione;
-	
-	@OneToMany(mappedBy="medico", cascade = {CascadeType.REFRESH, CascadeType.MERGE, CascadeType.PERSIST})
+	@Column(nullable = true, length = 64)
+	private String photos;
+
+	@Transient
+	public String getPhotosImagePath() {
+		if (photos == null || id == null)
+			return null;
+
+		return "/medico-photos/" + id + "/" + photos;
+	}
+
+	@OneToMany(mappedBy = "medico", cascade = { CascadeType.REFRESH, CascadeType.MERGE, CascadeType.PERSIST })
 	private List<Esame> esami;
 
 	@Override
@@ -37,7 +47,5 @@ public class Medico {
 		return "Medico [id=" + id + ", nome=" + nome + ", cognome=" + cognome + ", Specializzazione=" + Specializzazione
 				+ "]";
 	}
-
-	
 
 }
--- siw-spring-security/src/main/java/it/uniroma3/siw/spring/model/Tipologia.java
+++ siw-spring-security/src/main/java/it/uniroma3/siw/spring/model/Tipologia.java
@@ -1,5 +1,6 @@
 package it.uniroma3.siw.spring.model;
 
+import java.beans.Transient;
 import java.util.List;
 
 import javax.persistence.CascadeType;
@@ -19,23 +20,34 @@ public class Tipologia {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
-	
-	@Column(nullable=false)
+
+	@Column(nullable = false)
 	private String nome;
-	
-	@Column(nullable=false)
+
+	@Column(nullable = false)
 	private String descrizione;
-	
-	@Column(nullable=false)
-	private float costo; 
-	
-	
-	@OneToMany(mappedBy="tipologia",cascade = {CascadeType.REFRESH, CascadeType.MERGE, CascadeType.PERSIST})
+
+	@Column(nullable = false)
+	private float costo;
+	@Column(nullable = true, length = 64)
+	private String photos;
+
+	@Transient
+	public String getPhotosImagePath() {
+		if (photos == null || id == null)
+			return null;
+
+		return "/tipologia-photos/" + id + "/" + photos;
+	}
+
+	@OneToMany(mappedBy = "tipologia", cascade = { CascadeType.REFRESH, CascadeType.MERGE, CascadeType.PERSIST })
 	private List<Esame> esami;
-	
-	/* @OneToMany(mappedBy="tipologia",cascade=CascadeType.ALL)
-	private List <Prerequisito> prerequisiti; */
-	
+
+	/*
+	 * @OneToMany(mappedBy="tipologia",cascade=CascadeType.ALL) private List
+	 * <Prerequisito> prerequisiti;
+	 */
+
 	@ManyToMany
 	private List<Prerequisito> prerequisiti;
 
@@ -45,6 +57,4 @@ public class Tipologia {
 				+ ", prerequisito=" + prerequisiti + "]";
 	}
 
-	
-	
 }
